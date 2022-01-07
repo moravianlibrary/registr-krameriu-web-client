677edacdd3f2d20ed8df7907d4d310140da82a1f
Jan Rychtar <honza.rychtar@gmail.com>
2022-01-07 11:54:32 +0100

    prepinani jazyka

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DataService } from './services/data.service';
 import { environment } from '../environments/environment'
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +14,17 @@ export class AppComponent {
   loading: boolean = true;
   baseUrl = environment.baseUrl;
 
-  constructor(private http: HttpClient, private dataService: DataService) {}
+  constructor(private http: HttpClient, private translate: TranslateService, private dataService: DataService) {}
 
   ngOnInit() {
+    this.translate.use('cs');
+    // // Pouziti translate v kodu
+    // // 1 Asynchronne
+    // this.translate.get('table.cols.name').subscribe(a => {
+    //   console.log(a);
+    // });
+    // // 2 synchronne
+    // console.log(this.translate.instant('table.cols.name'));
     this.loading = true;
     this.http.get(this.baseUrl + '/libraries.json').subscribe((data: any) => {
       this.dataService.setData(data);
